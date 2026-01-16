bf6832144961e0e1cabd416db7aef5e4b4951533
25thOliver <os679736@gmail.com>
2026-01-17 02:31:55 +0300

    Enforce required fields

---
+++ src/validators.py
@@ -0,0 +1,12 @@
+from typing import Dict, Tuple
+from datetime import datetime
+
+REQUIRED_FIELDS = {
+    "product_id",
+    "title",
+    "image_path",
+    "image_url",
+    "price",
+    "rating",
+    "source",
+}
